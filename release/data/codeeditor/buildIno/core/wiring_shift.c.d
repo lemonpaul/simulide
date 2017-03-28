f49bbb89f84457f34e12b3deadc23b140582ba65
Arcachofo <25559964+Arcachofo@users.noreply.github.com>
2017-03-28 02:26:21 +0000

    Creating new trunk from simulide_0.0.4

---
+++ release/data/codeeditor/buildIno/core/wiring_shift.c.d
@@ -0,0 +1,6 @@
+/home/user/Escritorio/simulide_0.1.4/release/SimulIDE_0.1.4-Lin64-NO_PIC/data/codeeditor/buildIno/core/wiring_shift.c.o: \
+ /home/user/arduino-1.8.0/hardware/arduino/avr/cores/arduino/wiring_shift.c \
+ /home/user/arduino-1.8.0/hardware/arduino/avr/cores/arduino/wiring_private.h \
+ /home/user/arduino-1.8.0/hardware/arduino/avr/cores/arduino/Arduino.h \
+ /home/user/arduino-1.8.0/hardware/arduino/avr/cores/arduino/binary.h \
+ /home/user/arduino-1.8.0/hardware/arduino/avr/variants/standard/pins_arduino.h
