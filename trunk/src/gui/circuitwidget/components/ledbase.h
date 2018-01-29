d595620d2056847012fc2ca3ebd84b16d278fc59
arcachofo <arcachofo@01c38dc2-bffb-4c72-9ec5-e91a12afe270>
2018-01-29 12:45:07 +0000

    Added Led Color Property

--- trunk/src/gui/circuitwidget/components/ledbase.h
+++ trunk/src/gui/circuitwidget/components/ledbase.h
@@ -23,18 +23,34 @@
 #include "e-led.h"
 #include "e-source.h"
 #include "component.h"
+#include <QWidget>
+#include <QColor>
 
 class MAINMODULE_EXPORT LedBase : public Component, public eLed
 {
     Q_OBJECT
+    Q_PROPERTY( LedColor Color      READ color      WRITE setColor      DESIGNABLE true USER true )
     Q_PROPERTY( double threshold  READ threshold  WRITE setThreshold  DESIGNABLE true USER true )
     Q_PROPERTY( double MaxCurrent READ maxCurrent WRITE setMaxCurrent DESIGNABLE true USER true )
     Q_PROPERTY( double Resistance READ res        WRITE setRes        DESIGNABLE true USER true )
     Q_PROPERTY( bool   Grounded   READ grounded   WRITE setGrounded   DESIGNABLE true USER true )
+    Q_ENUMS( LedColor )
 
     public:
         LedBase( QObject* parent, QString type, QString id );
         ~LedBase();
+        
+        enum LedColor {
+            yellow = 0,
+            red,
+            green,
+            blue,
+            orange,
+            purple
+        };
+        
+        void setColor( LedColor color ) { m_ledColor = color; }
+        LedColor color() { return m_ledColor; } 
 
         void updateStep();
         
@@ -53,6 +69,9 @@ class MAINMODULE_EXPORT LedBase : public Component, public eLed
         bool     m_grounded;
         eSource* m_ground;
         eNode*   m_scrEnode;
+        
+        LedColor m_ledColor;
+        
 };
 
 #endif
