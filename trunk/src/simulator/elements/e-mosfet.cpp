f7a69ba45854055771a630055b9fdd68f0937bbd
arcachofo <arcachofo@01c38dc2-bffb-4c72-9ec5-e91a12afe270>
2018-02-08 08:35:35 +0000

    Mosfet: improbe  accuracy

--- trunk/src/simulator/elements/e-mosfet.cpp
+++ trunk/src/simulator/elements/e-mosfet.cpp
@@ -41,7 +41,7 @@ eMosfet::~eMosfet(){}
 
 void eMosfet::initialize()
 {
-    eResistor::setRes( 1 );
+    eResistor::setRes( m_RDSon );
     
     m_accuracy = Simulator::self()->NLaccuracy();
 
@@ -91,7 +91,7 @@ void eMosfet::setVChanged()
     
     if( m_gateV < 0 ) m_gateV = 0;
 
-    double satK = 1+Vds/100;
+    double satK = 1+Vds/150;
     double maxCurrDS = Vds/m_resist;
     
     if( Vds > m_gateV ) Vds = m_gateV;
