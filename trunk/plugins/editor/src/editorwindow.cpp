237c943bfc8d473ee1db814229399fd517c8660b
leamonpaul <leamonpaul@01c38dc2-bffb-4c72-9ec5-e91a12afe270>
2018-02-11 20:32:47 +0000

    Editor: Added shortcut hints.

--- trunk/plugins/editor/src/editorwindow.cpp
+++ trunk/plugins/editor/src/editorwindow.cpp
@@ -256,20 +256,20 @@ void EditorWindow::createWidgets()
 
 void EditorWindow::createActions()
 {
-    newAct = new QAction(QIcon(":/new.png"), tr("&New"), this);
+    newAct = new QAction(QIcon(":/new.png"), tr("&New\tCtrl+N"), this);
     newAct->setStatusTip(tr("Create a new file"));
     connect( newAct, SIGNAL(triggered()), this, SLOT(newFile()));
 
-    openAct = new QAction(QIcon(":/open.png"), tr("&Open..."), this);
+    openAct = new QAction(QIcon(":/open.png"), tr("&Open...\tCtrl+O"), this);
     openAct->setStatusTip(tr("Open an existing file"));
     connect(openAct, SIGNAL(triggered()), this, SLOT(open()));
 
-    saveAct = new QAction(QIcon(":/save.png"), tr("&Save"), this);
+    saveAct = new QAction(QIcon(":/save.png"), tr("&Save\tCtrl+S"), this);
     saveAct->setStatusTip(tr("Save the document to disk"));
     saveAct->setEnabled(false);
     connect(saveAct, SIGNAL(triggered()), this, SLOT(save()));
 
-    saveAsAct = new QAction(QIcon(":/saveas.png"),tr("Save &As..."), this);
+    saveAsAct = new QAction(QIcon(":/saveas.png"),tr("Save &As...\tCtrl+Shift+S"), this);
     saveAsAct->setStatusTip(tr("Save the document under a new name"));
     saveAsAct->setEnabled(false);
     connect(saveAsAct, SIGNAL(triggered()), this, SLOT(saveAs()));
@@ -278,27 +278,27 @@ void EditorWindow::createActions()
     exitAct->setStatusTip(tr("Exit the application"));
     connect(exitAct, SIGNAL(triggered()), this, SLOT(close()));
 
-    cutAct = new QAction(QIcon(":/cut.png"), tr("Cu&t"), this);
+    cutAct = new QAction(QIcon(":/cut.png"), tr("Cu&t\tCtrl+X"), this);
     cutAct->setStatusTip(tr("Cut the current selection's contents to the clipboard"));
     cutAct->setEnabled(false);
     connect(cutAct, SIGNAL(triggered()), this, SLOT(cut()));
 
-    copyAct = new QAction(QIcon(":/copy.png"), tr("&Copy"), this);
+    copyAct = new QAction(QIcon(":/copy.png"), tr("&Copy\tCtrl+C"), this);
     copyAct->setStatusTip(tr("Copy the current selection's contents to the clipboard"));
     copyAct->setEnabled(false);
     connect(copyAct, SIGNAL(triggered()), this, SLOT(copy()));
 
-    pasteAct = new QAction(QIcon(":/paste.png"), tr("&Paste"), this);
+    pasteAct = new QAction(QIcon(":/paste.png"), tr("&Paste\tCtrl+V"), this);
     pasteAct->setStatusTip(tr("Paste the clipboard's contents into the current selection"));
     pasteAct->setEnabled(false);
     connect(pasteAct, SIGNAL(triggered()), this, SLOT(paste()));
 
-    undoAct = new QAction(QIcon(":/undo.png"), tr("Undo"), this);
+    undoAct = new QAction(QIcon(":/undo.png"), tr("Undo\tCtrl+Z"), this);
     undoAct->setStatusTip(tr("Undo the last action"));
     undoAct->setEnabled(false);
     connect(undoAct, SIGNAL(triggered()), this, SLOT(undo()));
 
-    redoAct = new QAction(QIcon(":/redo.png"), tr("Redo"), this);
+    redoAct = new QAction(QIcon(":/redo.png"), tr("Redo\tCtrl+Shift+Z"), this);
     redoAct->setStatusTip(tr("Redo the last action"));
     redoAct->setEnabled(false);
     connect(redoAct, SIGNAL(triggered()), this, SLOT(redo()));
